e89f8e9aa8b129ed5730c864bdec13bba16fddea
rexlin <rexlin6245@gmail.com>
2021-06-27 18:27:23 -0700

    add new route idVerify, change chat route content, add main.hbs & index.hbs, add style sheet

--- routes/idVerify/idVerify.js
+++ routes/idVerify/idVerify.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.render('index')
+  console.log('here is verification')
 })
 
 module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const home = require('./home/home')
 const chatRoom = require('./chat/chatRoom')
+const idVerify = require('./idVerify/idVerify')
 
 const router = express.Router()
 
 router.use('/', home)
 router.use('/chat', chatRoom)
+router.use('/verify', idVerify)
 
 module.exports = router
